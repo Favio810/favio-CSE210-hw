7f3f8506def956480064d8dfbea512d46396290c
Favio <favioc810@gmail.com>
2024-05-04 20:51:58 -0400

    week 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int number = 0;
+        int attempts = 0;
+
+        while (attempts < 100)
+        {
+            while (number != magicNumber)
+            {
+                Console.Write("What is your guess?: ");
+                number = int.Parse(Console.ReadLine());
+
+                if (magicNumber > number)
+                {
+                    Console.WriteLine("Higher");
+                    attempts++;
+                }
+                else if (magicNumber < number)
+                {
+                    Console.WriteLine("Lower");
+                    attempts++;
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You did it in {attempts+1} attempts");
+                    break;
+                }
+            }
+        }    
     }
 }
