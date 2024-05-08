43aae6e541534ea0f11a6a815dd9856320019aed
Favio <favioc810@gmail.com>
2024-05-08 11:14:12 -0400

    week 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "General Surgeon";
+        job1._company = "Hospital Los Pinos";
+        job1._startYear = 2017;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "General Surgeon";
+        job2._company = "Clinica - Caja de la Banca Privada";
+        job2._startYear = 2019;
+        job2._endYear = 2021;
+
+        Job job3 = new Job();
+        job3._jobTitle = "General Surgeon";
+        job3._company = "Hospital Municipal de Caranavi";
+        job3._startYear = 2016;
+        job3._endYear = 2017;
+
+        //job1.DisplayJobDetails();
+        //Console.WriteLine("");
+        //Job2.DisplayJob();
+
+        Resume myResume = new Resume();
+        myResume._name = "Favio Callejas";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.DisplayResume();
     }
 }
