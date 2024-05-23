a05649935ef5d0157026fc03580f24d70e120aea
Favio <favioc810@gmail.com>
2024-05-22 23:06:34 -0400

    week 3

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment aa = new Assignment("Favio Callejas", "Multiplication and Division");
+        Console.WriteLine(aa.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment bb = new MathAssignment("Favio Callejas", "Addition and subtraction", "9.1", "9-21");
+        Console.WriteLine(bb.GetSummary());
+        Console.WriteLine(bb.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment cc = new WritingAssignment("Favio Callejas", "History of Bolivia", "Presidents of Bolivia");
+        Console.WriteLine(cc.GetSummary());
+        Console.WriteLine(cc.GetWritingInformation());
     }
 }
